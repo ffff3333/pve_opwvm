4baa136fef715bac002686030737bf23ea0643e9
sky777 <64050648+ffff3333@users.noreply.github.com>
2023-08-24 20:37:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git flrz https://github.com/flrz/flrz-package' feeds.conf.default
 # Add a feed source
-echo 'src-git flrz https://github.com/flrz/flrz-package' feeds.conf.default
+echo 'src-git flrz-package https://github.com/flrz/flrz-package' feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
