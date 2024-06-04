50f5eef9292b3b47c1e0cd9b43eb8cbacaed14f4
sky777 <64050648+ffff3333@users.noreply.github.com>
2024-06-04 09:26:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git argonee https://github.com/ffff3333/luci-theme-argonee' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/ffff3333/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
