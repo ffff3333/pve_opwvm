6174bb43e740966c5d9837909d2925bca36d85c8
sky777 <64050648+ffff3333@users.noreply.github.com>
2024-06-04 18:48:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,4 +23,4 @@ echo 'src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' 
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/ffff3333/openwrt-packages' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/ffff3333/packages' >>feeds.conf.default
