31892ae4743b0b7e43d898cba0334ee63c2f303d
god777 <64050648+ffff3333@users.noreply.github.com>
2024-12-30 10:44:46 +0800

    Update diy-part6.sh

--- diy-part6.sh
+++ diy-part6.sh
@@ -9,17 +9,5 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# Add a feed source
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-#echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-#echo 'src-git themes https://github.com/flrz/lede-themes' >>feeds.conf.default
-#echo 'src-git adguardhome https://github.com/flrz/adguardhome' >>feeds.conf.default
-#echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
