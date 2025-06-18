3a5ce4264010165190744504d56c9e7c5522694c
god777 <64050648+ffff3333@users.noreply.github.com>
2025-06-18 14:39:31 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -15,6 +15,6 @@
 # Add a feed source
 sed -i '1i packages https://github.com/ffff3333/packages' feeds.conf.default
 #sed -i '2i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
-#sed -i '3i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '3i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '4i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 sed -i '5i src-git themes https://github.com/flrz/openwrt-themes' feeds.conf.default
