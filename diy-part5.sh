5c761b0b992450b2cfde36982edd2a9731e4cb65
god777 <64050648+ffff3333@users.noreply.github.com>
2025-07-07 09:35:30 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -16,9 +16,10 @@
 #sed -i '1i src-git immortalwrt_packages https://github.com/immortalwrt/packages/tree/master/net' feeds.conf.default
 #sed -i '1i src-git passwall_packages https://github.com/ffff3333/openwrt-passwall-packages' feeds.conf.default
 #sed -i '1i src-git passwall_packages https://github.com/ffff3333/packages_updated' feeds.conf.default
-sed -i '2i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
-sed -i '3i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '4i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
-sed -i '5i src-git themes https://github.com/ffff3333/themes' feeds.conf.default
+sed -i '1i src-git flrz https://github.com/flrz/passwall_packages' feeds.conf.default
+#sed -i '2i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '3i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '4i src-git themes https://github.com/ffff3333/themes' feeds.conf.default
 #sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
