eea5a7bd900141d795dc76c672b98f9c573e4e3b
god777 <64050648+ffff3333@users.noreply.github.com>
2025-08-05 12:26:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,10 @@
 #sudo rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 #git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 #git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+#sudo apt update
+#sudo apt install build-essential clang flex bison g++ gawk \
+#gcc-multilib g++-multilib gettext git libncurses-dev libssl-dev \
+#python3-distutils python3-setuptools rsync swig unzip zlib1g-dev file wget
 # Add a feed source
 #sed -i '1i src-git immortalwrt_packages https://github.com/immortalwrt/packages' feeds.conf.default
 #sed -i '1i src-git passwall_packages https://github.com/ffff3333/openwrt-passwall-packages' feeds.conf.default
