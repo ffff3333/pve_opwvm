16c7ed73fe6ae7cf47c740f63f20c9f3e3e0f1f7
god777 <64050648+ffff3333@users.noreply.github.com>
2025-08-06 12:30:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 #1. Modify default IP
-#sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
+sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
 sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
