2050842c10c9d723602cdca9bd5469220508e81f
god777 <64050648+ffff3333@users.noreply.github.com>
2025-10-29 08:52:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,4 +24,4 @@
 #sed -i '1i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 #sed -i '2i src-git argon https://github.com/flrz/themes' feeds.conf.default
 #sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
