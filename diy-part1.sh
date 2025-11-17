1df35292ffdc59332f0633a01492beacb39cfa08
god777 <64050648+ffff3333@users.noreply.github.com>
2025-11-17 15:30:42 +0800

    Update feed sources in diy-part1.sh
    Comment out existing feed sources and add new ones.

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,8 @@
 # 移除 openwrt feeds 自带的核心库
 
 # Add a feed source
-sed -i '1i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
-sed -i '2i src-git flrz https://github.com/flrz/openwrt-packages' feeds.conf.default
-sed -i '3i src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki' feeds.conf.default
+#sed -i '1i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+#sed -i '2i src-git flrz https://github.com/flrz/openwrt-packages' feeds.conf.default
+#sed -i '3i src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki' feeds.conf.default
+sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
